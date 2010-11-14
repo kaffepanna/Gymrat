5981e6ce19210630c5dc593dfc2c8b34b6a36978
Patrik <pettersson.pa@gmail.com>
2010-11-14 11:22:29 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GymRat::Application.config.secret_token = '63cc46051e08f5eb58b2a1048021b9cb9a86a8cbae8657da8876ae8d52cbe30aa3b029054605de2c5231f2f1293258677b3bcab36476a7294e58ced3c28ac2bf'
